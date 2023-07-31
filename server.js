cd50e5ddc0193618c03f985b9da7e8c8b3ceddec
sohaib <akhtersohaib56@gmail.com>
2023-07-31 10:55:52 +0500

    first

---
+++ server.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const app=express();
+
+
+
+// routes 
+
+app.get('/',(req,res)=>{
+    res.send('hello Node api')
+})
+app.listen(3000,()=>{
+    console.log('Node api app is running on port 3000')
+})
