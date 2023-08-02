f8a5a31e4f1dfaafccfcc68e94249214a33601a3
sohaib <akhtersohaib56@gmail.com>
2023-08-02 15:13:42 +0500

    stil initial

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const mongoose= require('mongoose');
 const app=express();
 
 
@@ -8,6 +9,17 @@ const app=express();
 app.get('/',(req,res)=>{
     res.send('hello Node api')
 })
-app.listen(3000,()=>{
-    console.log('Node api app is running on port 3000')
+app.get('/blog',(req,res)=>{
+    res.send('hello blog my name is sohaib')
+})
+
+
+mongoose.connect('mongodb+srv://akhtersohaib56:abcd1234@sohaibapi.yiwliuw.mongodb.net/Node-API?retryWrites=true&w=majority').then(()=>{
+    console.log('connected to mongodb');
+    app.listen(3000,()=>{
+        console.log('Node api app is running on port 3000')
+    })    
+        
+}).catch((err)=>{
+    console.log("error connecting to db");
 })
